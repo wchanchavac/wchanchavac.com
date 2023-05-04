828c7a9b69a90cd02ee7a1dffd774ec56b30624f
wchanchavac <william.chanchavac@gmail.com>
2023-05-04 12:23:36 -0600

    Initial commit

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
+"use-strict";
+
 export default function handler(request, response) {
   const [subdomain] = request.subdomains;
   console.log(subdomain);
   if (subdomain == "meet") {
-    return res.redirect(process.env.MEET_URL);
+    return response.redirect(process.env.MEET_URL);
   }
-  return res.redirect(process.env.DEFAULT_URL);
+  return response.redirect(process.env.DEFAULT_URL);
 }
